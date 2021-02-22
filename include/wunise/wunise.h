40c8ee128d19b4819db204ab9b3f27e14c1e8608
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-02-23 01:38:10 +0800

    update interface

--- include/wunise/wunise.h
+++ include/wunise/wunise.h
@@ -2,4 +2,21 @@
 #ifndef __WUNISE__H__
 #define __WUNISE__H__
 
+namespace wunise {
+	
+	//Color
+	class Color {
+	public:
+		float r;
+		float b;
+		float g;
+		float a;
+	};
+	
+	//Mesh
+	class Mesh {
+
+	};
+}
+
 #endif // !__WUNISE__H__
