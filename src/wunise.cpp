ffcd0a2b734e401f0daed8b74275d746a872e838
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-03-01 03:31:44 +0800

    add shader

--- src/wunise.cpp
+++ src/wunise.cpp
@@ -5,6 +5,22 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
     wunise::Application app;
     app.InitWindow(hInstance, 300, 300);
+    const char* vertexShaderSource = "#version 450 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    const char* fragmentShaderSource = "#version 450 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+
+    wunise::GLObject globject;
+
+    globject.InitShader(vertexShaderSource, fragmentShaderSource);
 
     while (!app.WindowShouldClose()) {
 
