a9bdcf0e79c9464f5f5115f29d89841ef9f42c96
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-02-28 20:18:43 +0800

    UPDATE INTERFACE

--- include/wunise/application.h
+++ include/wunise/application.h
@@ -2,20 +2,24 @@
 #ifndef __APPLICATION__H__
 #define __APPLICATION__H__
 #include <Windows.h>
-#include <gl/GL.h>
 namespace wunise {
 	
 	class Application {
 	public:
 		Application() {
 			dc = NULL;
-			msg = {};
+			ZeroMemory(&msg, sizeof(MSG));
 		}
 		~Application() {}
 		Application(const Application& r) { dc = r.dc; msg = r.msg; }
-		Application(Application&&r) noexcept { dc = r.dc; msg = r.msg; }
+		Application(Application&&r) noexcept { 
+			dc = r.dc;
+			r.dc = NULL;
+			msg = r.msg;
+			ZeroMemory(&r.msg, sizeof(MSG));
+		}
 		Application& operator=(const Application&r) { dc = r.dc; msg = r.msg; return *this; }
-		Application& operator=(Application&&r) noexcept { dc = r.dc; msg = r.msg; return *this; }
+		Application& operator=(Application&&r) noexcept { dc = r.dc; r.dc = NULL; msg = r.msg; ZeroMemory(&r.msg, sizeof(MSG)); return *this; }
 
 		void InitWindow(HINSTANCE hInstance, int w, int h);
 		bool WindowShouldClose();
