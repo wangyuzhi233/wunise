8a5217efad33c9aa0d0863839086af40827b8394
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-03-09 19:38:37 +0800

    add

--- include/graphics.h
+++ include/graphics.h
@@ -1,29 +1,19 @@
 #pragma once
 #ifndef __GRAPHICS_H__
 #define __GRAPHICS_H__
-#include <Windows.h>
-#include <d3d11_4.h>
-#include <dxgi1_6.h>
-#include <wrl/client.h>
+
 namespace wunise {
 	class GraphicsDevice {
 	public:
-		GraphicsDevice();
-		~GraphicsDevice();
-		GraphicsDevice(const GraphicsDevice&);
-		GraphicsDevice(GraphicsDevice&&) noexcept;
-		GraphicsDevice& operator=(const GraphicsDevice&);
-		GraphicsDevice& operator=(GraphicsDevice&&) noexcept;
+		GraphicsDevice() {}
+		~GraphicsDevice() = default;
+		
+		GraphicsDevice(GraphicsDevice&&) noexcept = default;
+		GraphicsDevice& operator=(GraphicsDevice&&) noexcept = default;
 
-		void CreateDeivce();
-		void CreateSwapChain(HWND hwnd,int width,int height);
-		void Present();
+		GraphicsDevice(const GraphicsDevice&) = delete;
+		GraphicsDevice& operator=(const GraphicsDevice&) = delete;
 	private:
-		Microsoft::WRL::ComPtr<ID3D11Device1>           m_d3dDevice;
-		Microsoft::WRL::ComPtr<ID3D11DeviceContext1>    m_d3dContext;
-		Microsoft::WRL::ComPtr<IDXGISwapChain1>         m_swapChain;
-		Microsoft::WRL::ComPtr<ID3D11RenderTargetView>  m_renderTargetView;
-		Microsoft::WRL::ComPtr<ID3D11DepthStencilView>  m_depthStencilView;
 	};
 }
 #endif // !__GRAPHICS_H__
--- src/wunise.cpp
+++ src/wunise.cpp
@@ -1,63 +1,9 @@
 #include <Windows.h>
-#include "game.h"
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-    wunise::Game g_game;
-    // Register class and create window
-    {
-        // Register class
-        WNDCLASSEXW wcex = {};
-        wcex.cbSize = sizeof(WNDCLASSEXW);
-        wcex.style = CS_HREDRAW | CS_VREDRAW;
-        wcex.lpfnWndProc = wunise::window::WndProc;
-        wcex.hInstance = hInstance;
-        wcex.hIcon = LoadIconW(hInstance, L"IDI_ICON");
-        wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
-        wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
-        wcex.lpszClassName = L"$safeprojectname$WindowClass";
-        wcex.hIconSm = LoadIconW(wcex.hInstance, L"IDI_ICON");
-        if (!RegisterClassExW(&wcex))
-            return 1;
 
-        // Create window
-        int w, h;
-        g_game.GetDefaultSize(w, h);
-
-        RECT rc = { 0, 0, static_cast<LONG>(w), static_cast<LONG>(h) };
-
-        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-
-        HWND hwnd = CreateWindowExW(0, L"$safeprojectname$WindowClass", L"$projectname$", WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
-            nullptr);
-        // TODO: Change to CreateWindowExW(WS_EX_TOPMOST, L"$safeprojectname$WindowClass", L"$projectname$", WS_POPUP,
-        // to default to fullscreen.
-
-        if (!hwnd)
-            return 1;
-
-        ShowWindow(hwnd, nCmdShow);
-        // TODO: Change nCmdShow to SW_SHOWMAXIMIZED to default to fullscreen.
-
-        SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(&g_game));
-        g_game.Initialize(hwnd);
-    }
-    // Main message loop
-    MSG msg = {};
-    while (WM_QUIT != msg.message)
-    {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else
-        {
-            g_game.Tick();
-        }
-    }
-    return static_cast<int>(msg.wParam);
+    return 0;
 }
 
 
